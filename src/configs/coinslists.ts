daf918303bffdca1d33c105807ebfcae1913831e
Vintizer <vizerkharkiv@gmail.com>
2022-06-30 00:27:25 +0300

    part work

--- src/configs/coinslists.ts
+++ src/configs/coinslists.ts
@@ -1,25 +1,35 @@
 import { CoinsList } from "../interfaces";
 
-export const coinsLists: CoinsList[] = [
+const binanceCoinsList = [
   {
     name: "GOLD",
-    fullList:
-      "Ethereum (ETH),Bitcoin Cash (BCH),XRP (XRP),EOS (EOS),Litecoin (LTC),TRON (TRX),Ethereum Classic (ETC),Chainlink (LINK),Stellar (XLM),Cardano (ADA),Monero (XMR),DASH (DASH),Zcash (ZEC),Tezos (XTZ),Binance Coin (BNB),Cosmos (ATOM),Ontology (ONT),IOTA (IOTA),Basic Attention Token (BAT),VeChain (VET),Neo (NEO),Qtum (QTUM),IOST (IOST),THETA (THETA),Algorand (ALGO),Zilliqa (ZIL),Kyber Network (KNC),0x (ZRX),Compound Coin (COMP),OMG Network (OMG),Dogecoin (DOGE),Swipe (SXP),(KAVA),Band Protocol (BAND),iExec RLC (RLC),Waves (WAVES),Maker (MKR),Synthetix Network Token (SNX),DOT (DOT),DEFI (DEFI),Balancer (BAL),Curve DAO Token (CRV),YFII (YFII),Serum (SRM),bZx Protocol (BZRX),ICON (ICX),Storj (STORJ),Bluzelle (BLZ),Fantom (FTM),Enjin Coin (ENJ),FLM (FLM),TomoChain (TOMO),Ren (REN),Reserve Rights (RSR),Matic Network (MATIC),Ocean Protocol (OCEAN),Civic (CVC),BEL (BEL),CTK (CTK),ALPHA (ALPHA),Horizen (ZEN),SKL (SKL),GRT (GRT),1INCH (1INCH),Akropolis (AKRO),Chiliz (CHZ),The Sandbox (SAND),Ankr (ANKR),BitShares (BTS),LITHIUM (LIT),UNFI (UNFI),DODO (DODO),REEF (REEF),Ravencoin (RVN),SFP (SFP),NEM (XEM),COTI (COTI),Chromia (CHR),Decentraland (MANA),ALICE (ALICE),Hedera Hashgraph (HBAR),BigONE Token (ONE),LINA (LINA),StormX (STMX),Dent (DENT),Celer Network (CELR),Hydro Protocol (HOT),Metal (MTL),Origin Protocol (OGN),BitTorrent (BTT),NKN (NKN),Siacoin (SC),DigiByte (DGB),1000SHIB (1000SHIB),ICP (ICP),BAKE (BAKE),GTC (GTC),BTCDOM (BTCDOM),KEEP (KEEP),TALIUM (TLM),IoTeX (IOTX),AUDIO (AUDIO),RAY (RAY),C98 (C98),ATA (ATA),FarmaTrust (FTT),DYDX (DYDX),1000XEC (1000XEC),GALA (GALA),CELO (CELO),AR (AR),KLAY (KLAY),ARPA Chain (ARPA),NU (NU),Cartesi (CTSI)",
     simpleList:
-      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ATOM),(ONT),(IOTA),(BAT),(VET),(NEO),(QTUM),(IOST),(THETA),(ALGO),(ZIL),(KNC),(ZRX),(COMP),(OMG),(DOGE),(SXP),(KAVA),(BAND),(RLC),(WAVES),(MKR),(SNX),(DOT),(DEFI),(BAL),(CRV),(YFII),(SRM),(BZRX),(ICX),(STORJ),(BLZ),(FTM),(ENJ),(FLM),(TOMO),(REN),(RSR),(MATIC),(OCEAN),(CVC),(BEL),(CTK),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(AKRO),(CHZ),(SAND),(ANKR),(BTS),(LIT),(UNFI),(DODO),(REEF),(RVN),(SFP),(XEM),(COTI),(CHR),(MANA),(ALICE),(HBAR),(ONE),(LINA),(STMX),(DENT),(CELR),(HOT),(MTL),(OGN),(BTT),(NKN),(SC),(DGB),(1000SHIB),(ICP),(BAKE),(GTC),(BTCDOM),(KEEP),(TLM),(IOTX),(AUDIO),(RAY),(C98),(ATA),(FTT),(DYDX),(1000XEC),(GALA),(CELO),(AR),(KLAY),(ARPA),(NU),(CTSI)",
+      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ATOM),(ONT),(IOTA),(BAT),(VET),(NEO),(QTUM),(IOST),(THETA),(ALGO),(ZIL),(KNC),(ZRX),(COMP),(OMG),(DOGE),(SXP),(KAVA),(BAND),(RLC),(WAVES),(MKR),(SNX),(DOT),(DEFI),(BAL),(CRV),(SRM),(ICX),(STORJ),(BLZ),(FTM),(ENJ),(FLM),(TOMO),(REN),(RSR),(LRC),(MATIC),(OCEAN),(CVC),(BEL),(CTK),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(BTS),(LIT),(UNFI),(REEF),(RVN),(SFP),(XEM),(COTI),(CHR),(MANA),(ALICE),(HBAR),(ONE),(LINA),(STMX),(DENT),(CELR),(HOT),(MTL),(OGN),(NKN),(DGB),(1000SHIB),(BAKE),(GTC),(BTCDOM),(IOTX),(RAY),(C98),(ATA),(FTT),(DYDX),(1000XEC),(GALA),(CELO),(AR),(KLAY),(ARPA),(CTSI),(LPT),(ENS),(PEOPLE),(ANT),(ROSE),(DUSK),(FLOW),(IMX),(API3),(GMT),(APE),(BNX),(WOO),(JASMY),(DAR),(GAL),(OP)",
   },
   {
     name: "GOLD MID",
-    fullList:
-      "Ethereum (ETH),Bitcoin Cash (BCH),XRP (XRP),EOS (EOS),Litecoin (LTC),TRON (TRX),Ethereum Classic (ETC),Chainlink (LINK),Stellar (XLM),Cardano (ADA),Monero (XMR),DASH (DASH),Zcash (ZEC),Tezos (XTZ),Cosmos (ATOM),Neo (NEO),THETA (THETA),Algorand (ALGO),Zilliqa (ZIL),Kyber Network (KNC),0x (ZRX),Compound Coin (COMP),Band Protocol (BAND),Waves (WAVES),Maker (MKR),Synthetix Network Token (SNX),DOT (DOT),Balancer (BAL),Curve DAO Token (CRV),Serum (SRM),bZx Protocol (BZRX),Fantom (FTM),Enjin Coin (ENJ),TomoChain (TOMO),Matic Network (MATIC),ALPHA (ALPHA),SKL (SKL),GRT (GRT),1INCH (1INCH),Chiliz (CHZ),NEM (XEM),COTI (COTI),ALICE (ALICE),Hedera Hashgraph (HBAR),Hydro Protocol (HOT),Metal (MTL),ICP (ICP),BAKE (BAKE),GTC (GTC),C98 (C98)",
     simpleList:
       "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(ATOM),(NEO),(THETA),(ALGO),(ZIL),(KNC),(ZRX),(COMP),(BAND),(WAVES),(MKR),(SNX),(DOT),(BAL),(CRV),(SRM),(BZRX),(FTM),(ENJ),(TOMO),(MATIC),(ALPHA),(SKL),(GRT),(1INCH),(CHZ),(XEM),(COTI),(ALICE),(HBAR),(HOT),(MTL),(ICP),(BAKE),(GTC),(C98)",
   },
   {
     name: "GOLD DEF",
-    fullList:
-      "Ethereum (ETH),Bitcoin Cash (BCH),XRP (XRP),EOS (EOS),Litecoin (LTC),TRON (TRX),Ethereum Classic (ETC),Chainlink (LINK),Stellar (XLM),Cardano (ADA),Monero (XMR),DASH (DASH),Zcash (ZEC),Cosmos (ATOM),Neo (NEO),THETA (THETA),Algorand (ALGO),Kyber Network (KNC),Compound Coin (COMP),DOT (DOT),Enjin Coin (ENJ),TomoChain (TOMO),ALPHA (ALPHA),SKL (SKL)",
     simpleList:
-      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(ATOM),(NEO),(THETA),(ALGO),(KNC),(COMP),(DOT),(ENJ),(TOMO),(ALPHA),(SKL)",
+      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ATOM),(BAT),(VET),(NEO),(QTUM),(THETA),(ALGO),(ZIL),(ZRX),(COMP),(OMG),(SXP),(MKR),(SNX),(DOT),(DEFI),(BAL),(CRV),(SRM),(STORJ),(FTM),(ENJ),(TOMO),(REN),(MATIC),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(XEM),(MANA),(ALICE),(HOT),(MTL),(BAKE),(CTSI)",
   },
 ];
+const bybitCoinsList = [
+  {
+    name: "GOLD",
+    simpleList:
+      "(EOS),(XRP),(XTZ),(LINK),(ADA),(DOT),(XEM),(DOGE),(MATIC),(FIL),(XLM),(VET),(THETA),(COMP),(MANA),(ATOM),(CHZ),(CRV),(ENJ),(GRT),(SHIB1000),(ICP),(FTM),(ALGO),(DYDX),(SRM),(BIT),(GALA),(HBAR),(ONE),(C98),(ALICE),(REN),(TLM),(FLOW),(LRC),(ENS),(IOTX),(CHR),(BAT),(STORJ),(SNX),(ANKR),(QTUM),(CRO),(SXP),(ZEC),(SFP),(IOTA),(WAVES),(KNC),(OGN),(BSW),(CTSI),(HOT),(DGB),(GLMR),(BAKE),(MTL),(CVX),(DODO),(TOMO),(GST),(DAR),(GAL),(FITFI),(AKRO),(OP)",
+  },
+  {
+    name: "GOLD DEF",
+    simpleList:
+      "(EOS),(XRP),(XTZ),(LINK),(ADA),(DOT),(XEM),(MATIC),(FIL),(XLM),(VET),(THETA),(COMP),(MANA),(ATOM),(CHZ),(CRV),(ENJ),(GRT),(SHIB1000),(ICP),(FTM),(ALGO),(DYDX),(SRM),(FTT),(HBAR),(ONE),(C98),(ALICE),(REN),(BAT),(STORJ),(ANKR),(CRO),(ZEC),(WAVES),(OGN),(HOT),(BAKE),(TOMO)",
+  },
+];
+export const coinsLists: Record<string, CoinsList[]> = {
+  Binance: binanceCoinsList,
+  Bybit: bybitCoinsList,
+};
