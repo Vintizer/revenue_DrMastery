51873162735cd89720402dfaca46c37ff2ac1bea
Vintizer <vizerkharkiv@gmail.com>
2022-07-05 23:28:00 +0300

    1.13.2

--- src/configs/coinslists.ts
+++ src/configs/coinslists.ts
@@ -4,12 +4,12 @@ const binanceCoinsList = [
   {
     name: "aggressive",
     simpleList:
-      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ATOM),(ONT),(IOTA),(BAT),(VET),(NEO),(QTUM),(IOST),(THETA),(ALGO),(ZIL),(KNC),(ZRX),(COMP),(OMG),(DOGE),(SXP),(KAVA),(BAND),(RLC),(WAVES),(MKR),(SNX),(DOT),(DEFI),(BAL),(CRV),(SRM),(ICX),(STORJ),(BLZ),(FTM),(ENJ),(FLM),(TOMO),(REN),(RSR),(LRC),(MATIC),(OCEAN),(CVC),(BEL),(CTK),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(BTS),(LIT),(UNFI),(REEF),(RVN),(SFP),(XEM),(COTI),(CHR),(MANA),(ALICE),(HBAR),(ONE),(LINA),(STMX),(DENT),(CELR),(HOT),(MTL),(OGN),(NKN),(DGB),(1000SHIB),(BAKE),(GTC),(BTCDOM),(IOTX),(RAY),(C98),(ATA),(FTT),(DYDX),(1000XEC),(GALA),(CELO),(AR),(KLAY),(ARPA),(CTSI),(LPT),(ENS),(PEOPLE),(ANT),(ROSE),(DUSK),(FLOW),(IMX),(API3),(GMT),(APE),(BNX),(WOO),(JASMY),(DAR),(GAL),(OP)",
+      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ATOM),(ONT),(IOTA),(BAT),(VET),(NEO),(QTUM),(IOST),(THETA),(ALGO),(ZIL),(KNC),(ZRX),(COMP),(OMG),(SXP),(KAVA),(WAVES),(MKR),(SNX),(DOT),(DEFI),(BAL),(CRV),(SRM),(ICX),(STORJ),(FTM),(ENJ),(TOMO),(REN),(RSR),(MATIC),(OCEAN),(CVC),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(RVN),(XEM),(COTI),(CHR),(MANA),(ALICE),(HBAR),(ONE),(CELR),(HOT),(OGN),(DGB),(1000SHIB),(BAKE),(BTCDOM),(IOTX),(FTT),(DYDX),(1000XEC),(GALA),(CELO),(AR),(KLAY),(CTSI),(LPT),(ENS),(PEOPLE),(ROSE),(FLOW),(IMX),(GMT)",
   },
   {
     name: "aggressivex50",
     simpleList:
-      "(ETH),(BCH),(XRP),(EOS),(LTC),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ONT),(IOTA),(BAT),(NEO),(QTUM),(IOST),(THETA),(KNC),(ZRX),(COMP),(DOGE),(SXP),(KAVA),(BAND),(RLC),(WAVES),(MKR),(DOT),(DEFI),(BAL),(CRV),(SRM),(ICX),(STORJ),(ENJ),(TOMO),(REN),(MATIC),(OCEAN),(CVC),(CTK),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(BTS),(REEF),(RVN),(SFP),(XEM),(COTI),(CHR),(MANA),(HBAR),(LINA),(STMX),(DENT),(HOT),(MTL),(NKN),(DGB),(1000SHIB),(BAKE),(GTC),(GMT),(APE),(BNX)",
+      "(ETH),(BCH),(XRP),(EOS),(LTC),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ONT),(IOTA),(BAT),(NEO),(QTUM),(IOST),(THETA),(KNC),(ZRX),(COMP),(SXP),(KAVA),(BAND),(WAVES),(MKR),(DOT),(DEFI),(BAL),(CRV),(SRM),(ICX),(STORJ),(ENJ),(TOMO),(REN),(MATIC),(OCEAN),(CVC),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(RVN),(XEM),(COTI),(CHR),(MANA),(HBAR),(HOT),(MTL),(DGB),(1000SHIB),(BAKE),(GMT)",
   },
   {
     name: "standart",
@@ -19,19 +19,19 @@ const binanceCoinsList = [
   {
     name: "safe",
     simpleList:
-      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ATOM),(BAT),(VET),(NEO),(QTUM),(THETA),(ALGO),(ZIL),(ZRX),(COMP),(OMG),(SXP),(MKR),(SNX),(DOT),(DEFI),(BAL),(CRV),(SRM),(STORJ),(FTM),(ENJ),(TOMO),(REN),(MATIC),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(XEM),(MANA),(ALICE),(HOT),(MTL),(BAKE),(CTSI)",
+      "(ETH),(BCH),(XRP),(EOS),(LTC),(TRX),(ETC),(LINK),(XLM),(ADA),(XMR),(DASH),(ZEC),(XTZ),(BNB),(ATOM),(BAT),(VET),(NEO),(QTUM),(THETA),(ALGO),(ZIL),(ZRX),(COMP),(OMG),(SXP),(MKR),(SNX),(DOT),(DEFI),(BAL),(CRV),(SRM),(STORJ),(FTM),(ENJ),(TOMO),(REN),(MATIC),(ALPHA),(ZEN),(SKL),(GRT),(1INCH),(CHZ),(SAND),(ANKR),(XEM),(MANA),(ALICE),(HOT),(BAKE),(CTSI)",
   },
 ];
 const bybitCoinsList = [
   {
     name: "aggressive",
     simpleList:
-      "(EOS),(XRP),(XTZ),(LINK),(ADA),(DOT),(XEM),(DOGE),(MATIC),(FIL),(XLM),(VET),(THETA),(COMP),(MANA),(ATOM),(CHZ),(CRV),(ENJ),(GRT),(SHIB1000),(ICP),(FTM),(ALGO),(DYDX),(SRM),(BIT),(GALA),(HBAR),(ONE),(C98),(ALICE),(REN),(TLM),(FLOW),(LRC),(ENS),(IOTX),(CHR),(BAT),(STORJ),(SNX),(ANKR),(QTUM),(CRO),(SXP),(ZEC),(SFP),(IOTA),(WAVES),(KNC),(OGN),(BSW),(CTSI),(HOT),(DGB),(GLMR),(BAKE),(MTL),(CVX),(DODO),(TOMO),(GST),(DAR),(GAL),(FITFI),(AKRO),(OP)",
+      "(EOS),(XRP),(XTZ),(LINK),(ADA),(DOT),(XEM),(DOGE),(MATIC),(FIL),(XLM),(VET),(THETA),(COMP),(MANA),(ATOM),(CHZ),(CRV),(ENJ),(GRT),(SHIB1000),(ICP),(FTM),(ALGO),(SRM),(BIT),(GALA),(HBAR),(ONE),(ALICE),(REN),(TLM),(FLOW),(IOTX),(CHR),(BAT),(STORJ),(SNX),(ANKR),(QTUM),(CRO),(SXP),(ZEC),(IOTA),(WAVES),(KNC),(OGN),(BSW),(CTSI),(HOT),(DGB),(BAKE),(CVX),(DODO),(TOMO),(AKRO)",
   },
   {
     name: "safe",
     simpleList:
-      "(EOS),(XRP),(XTZ),(LINK),(ADA),(DOT),(XEM),(MATIC),(FIL),(XLM),(VET),(THETA),(COMP),(MANA),(ATOM),(CHZ),(CRV),(ENJ),(GRT),(SHIB1000),(ICP),(FTM),(ALGO),(DYDX),(SRM),(FTT),(HBAR),(ONE),(C98),(ALICE),(REN),(BAT),(STORJ),(ANKR),(CRO),(ZEC),(WAVES),(OGN),(HOT),(BAKE),(TOMO)",
+      "(EOS),(XRP),(XTZ),(LINK),(ADA),(DOT),(XEM),(MATIC),(XLM),(VET),(THETA),(COMP),(MANA),(ATOM),(CHZ),(CRV),(ENJ),(ALGO),(REN),(BAT),(STORJ),(CRO),(ZEC),(HOT),(BAKE)",
   },
 ];
 export const coinsLists: Record<string, CoinsList[]> = {
--- src/configs/wallets.ts
+++ src/configs/wallets.ts
@@ -3,18 +3,18 @@ import { Wallet } from "../interfaces";
 export const walletsFront2: Wallet[] = [
   { depo: 10, wallet: 1, leverage: 20 },
   { depo: 15, wallet: 1, leverage: 20 },
-  { depo: 20, wallet: 2, leverage: 20 },
+  { depo: 20, wallet: 1, leverage: 20 },
   { depo: 50, wallet: 2, leverage: 20 },
   { depo: 100, wallet: 3, leverage: 20 },
   { depo: 200, wallet: 3, leverage: 20 },
   { depo: 300, wallet: 3, leverage: 20 },
   { depo: 500, wallet: 5, leverage: 20 },
-  // exception500 Binance === 7.5
   { depo: 1000, wallet: 10, leverage: 20 },
   { depo: 2000, wallet: 20, leverage: 20 },
   { depo: 3000, wallet: 30, leverage: 20 },
   { depo: 5000, wallet: 50, leverage: 20 },
   { depo: 10000, wallet: 100, leverage: 20 },
+  // exception500 Binance === 7.5
   { depo: 500, wallet: 7.5, leverage: 50 },
   { depo: 1000, wallet: 10, leverage: 50 },
   { depo: 2000, wallet: 20, leverage: 50 },
